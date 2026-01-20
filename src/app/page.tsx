2a8b525ff331a58a17e7a7901da776bb55daa150
shaikh abdul wahhab <adshaikh837@gmail.com>
2026-01-20 20:03:28 +0530

    use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 const Homepage = () => {
+  useEffect(() => {
+    redirect("/admin");
+    
+  }, [])
+  
   return (
+    
     <div className=''>Homepage</div>
   )
 }
